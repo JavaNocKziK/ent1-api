7d92ed92f9c17e498c9e53183ab4e85a5291bf79
JavaNocKziK <javanockzik@googlemail.com>
2018-01-12 01:50:57 +0000

    CORS update.

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ const port = process.env.PORT || 8200;
 mongoose.connect('mongodb://ent1admin:67YraCgjvE7u4gE6@ds135817.mlab.com:35817/uni3-ent1');
 //mongoose.connect('mongodb://localhost:27017/');
 
+// Set CORS to allow all remote addresses to access our API.
 const CORS = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 }
 
-//app.use(CORS);
+app.use(CORS);
 
 app.use(bodyParser.urlencoded(
     {
@@ -29,4 +30,4 @@ app.use(bodyParser.json());
 app.use('/films', filmroutes);
 
 app.listen(port);
-console.log(`Magic happens on port ${port}`);
+console.log(`Listening on ${port}...`);
