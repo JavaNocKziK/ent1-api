c1341dd202d0abbbdac3a5bd78274c89c0effbca
JavaNocKziK <javanockzik@googlemail.com>
2018-01-12 10:31:32 +0000

    Added ability to declare port in command line.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const filmroutes = require('./app/routes/films');
 
 const app = express();
-const port = process.env.PORT || 8200;
+const port = process.env.PORT || process.argv[2] || 8200;
 
 mongoose.connect('mongodb://ent1admin:67YraCgjvE7u4gE6@ds135817.mlab.com:35817/uni3-ent1');
 //mongoose.connect('mongodb://localhost:27017/');
